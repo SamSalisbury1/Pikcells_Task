a592cbb925584e074545c93d703cdb79e9ff3491
SamSalisbury1 <samualsalisbury@gmail.com>
2020-10-07 21:35:34 +0100

    Implemented JQuery and most functionality for user interface, need to refactor code and add final functionality.

--- TaskCode/Main.js
+++ TaskCode/Main.js
@@ -1,3 +1,5 @@
+import $ from "./jquery.module.js";
+
 const JSON_URL = "https://lab.pikcells.com/code-exercise/data.json";
 const IMAGE_PREFIX = "https://lab.pikcells.com/code-exercise/images/";
 main();
@@ -6,7 +8,9 @@ function main() {
     const kitchenData = JSON.parse(GetJSON(JSON_URL));
     OrderData(kitchenData);
     ShowDefaultKitchen(kitchenData);
-
+    ShowDefaultDesignChoices(kitchenData);
+    PopulateDropDownLists(kitchenData);
+    console.log(kitchenData);
 }
 
 function GetJSON(JSON_URL) {
@@ -24,7 +28,7 @@ function OrderData(kitchenData) {
 }
 
 function ShowDefaultKitchen(kitchenData) {
-    const canvas = document.getElementById('kitchenCanvas'),
+    const canvas = document.getElementById('kitchen_Canvas'),
     context = canvas.getContext('2d');
     const defaultImages = GetDefaultImages(kitchenData);
     
@@ -61,3 +65,98 @@ function GetDefaultImageSources(kitchenData) {
 
     return defaultImageSources;
 }
+
+function ShowDefaultDesignChoices(kitchenData) {
+    const CurrentDesignChoices = GetSelecteRangeNames(kitchenData);
+    const designChoices = document.getElementById("Kitchen_Choices");
+    designChoices.innerHTML = 
+        CurrentDesignChoices[0] + ", " +
+        CurrentDesignChoices[1] + ", " + 
+        CurrentDesignChoices[2]; 
+}
+
+function GetSelecteRangeNames(kitchenData) {
+    const defaultConfiguration = kitchenData.default_configuration;
+    const defaultLayerName = [];
+
+    for (let i = 0; i < kitchenData.layers.length; i++) {
+            const currentLayer = kitchenData.layers[i];
+            const configurationIndex = defaultConfiguration[i];
+            const LayerName = currentLayer.items[configurationIndex].name;
+            defaultLayerName.push(LayerName);
+    }
+
+    return defaultLayerName;
+}
+
+function PopulateDropDownLists(kitchenData) {
+    const layers = kitchenData.layers;
+
+    layers[0].items.forEach(item => {
+        $("#Layer_1_Select").append(new Option(item.name, item.imgSrc))
+    })
+
+    layers[1].items.forEach(item => {
+        $("#Layer_2_Select").append(new Option(item.name, item.imgSrc))
+    })
+
+    layers[2].items.forEach(item => {
+        $("#Layer_3_Select").append(new Option(item.name, item.imgSrc))
+    })
+
+    SetDefaultDropDownOptions(kitchenData);
+}
+
+function SetDefaultDropDownOptions(kitchenData) {
+    const a = GetSelecteRangeNames(kitchenData);
+
+    
+}
+
+document.getElementById("Update_Button").onclick = function() {
+    UpdateCanvas();
+    UpdateDesignChoices();
+    
+}
+
+function UpdateCanvas() { // TODO Put in render and merge with show default kitchen 
+    const canvas = document.getElementById('kitchen_Canvas'),
+    context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const images = GetSelectedImages();
+
+    images.forEach(image => {
+        image.onload = function() {
+            context.drawImage(image, 0, 0)
+        }
+    })
+}
+
+function UpdateDesignChoices() { //TO DO
+    $("p#Kitchen_Choices").text(
+        
+    );
+}
+
+function GetSelectedImages() {
+    const imageSources = GetSelectedImageSources();
+    const images = [];
+
+    imageSources.forEach(source => {
+        const currentImage = new Image();
+        currentImage.src = source;
+        images.push(currentImage);
+    });
+
+    return images;
+}
+
+function GetSelectedImageSources() {
+    return [
+        IMAGE_PREFIX + $("#Layer_1_Select").val(),
+        IMAGE_PREFIX + $("#Layer_2_Select").val(),
+        IMAGE_PREFIX + $("#Layer_3_Select").val()
+    ]
+}
+
+// TODO Refactor much of this code and add final bits of functionality.
