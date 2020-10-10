abdfc5a227e68aafd25dcda4f79a2a5309bbf00a
SamSalisbury1 <samualsalisbury@gmail.com>
2020-10-10 16:45:48 +0100

    Give user a warning if they're using IE.

--- TaskCode/Main.js
+++ TaskCode/Main.js
@@ -1,8 +1,15 @@
+import $ from "./jquery.module.js";
 import { initialiser } from "./Initialiser.js";
 import { uiHandler } from "./UIHandler.js";
 import { saveHandler } from "./SaveHandler.js";
+
 class Main {
     main() {
+        // If user is using IE Take them to Not Supported Page.
+        if (navigator.appVersion.indexOf("MSIE")!=-1) {
+            window.location.href = "./NotSupported.html";
+        }
+
         initialiser.Initialise();
     }
 }
